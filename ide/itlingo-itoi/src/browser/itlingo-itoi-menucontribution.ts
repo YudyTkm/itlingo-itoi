3ed6286d571619054e4ca799509bca5b59b4e4a3
genlike <ricardo.f.oliveira.da.silva@tecnico.ulisboa.pt>
2023-07-20 16:01:02 +0100

    remove open workspace and close workspace

--- ide/itlingo-itoi/src/browser/itlingo-itoi-menucontribution.ts
+++ ide/itlingo-itoi/src/browser/itlingo-itoi-menucontribution.ts
@@ -115,6 +115,8 @@ export class TheiaExampleCommandContribution implements CommandContribution {
         commands.unregisterCommand(GIT_COMMANDS.CLONE);
         commands.unregisterCommand(GIT_COMMANDS.FETCH);
         commands.unregisterCommand(WorkspaceCommands.OPEN_WORKSPACE);
+        commands.unregisterCommand(WorkspaceCommands.OPEN_RECENT_WORKSPACE);
+        commands.unregisterCommand(WorkspaceCommands.ADD_FOLDER);
         commands.unregisterCommand(WorkspaceCommands.OPEN_FOLDER);
         commands.unregisterCommand(WorkspaceCommands.CLOSE);
         
@@ -123,7 +125,8 @@ export class TheiaExampleCommandContribution implements CommandContribution {
         commands.registerCommand(WorkspaceCommands.OPEN_WORKSPACE, {isEnabled:()=>{ return false}, execute:()=>{}});
         commands.registerCommand(WorkspaceCommands.OPEN_FOLDER, {isEnabled:()=>{ return false}, execute:()=>{}});
         commands.registerCommand(WorkspaceCommands.CLOSE, {isEnabled:()=>{ return false}, execute:()=>{}});
-
+        commands.registerCommand(WorkspaceCommands.OPEN_RECENT_WORKSPACE, {isEnabled:()=>{ return false}, execute:()=>{}});
+        commands.registerCommand(WorkspaceCommands.ADD_FOLDER, {isEnabled:()=>{ return false}, execute:()=>{}});
 
         GIT_MENUS.SUBMENU_PULL_PUSH.label = "Extended Actions";
         GIT_COMMANDS.FETCH.label = "Clone...";
